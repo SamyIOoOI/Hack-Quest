ed2cf84b487fcb5a6dd41dc56ddcf4f2a6df3c1d
SamyIOoOI <samytonygo@gmail.com>
2025-10-03 15:15:38 +0300

    clear command and welcome message

--- Main_Hack.py
+++ Main_Hack.py
@@ -1,5 +1,9 @@
 from rich import print
 from rich.console import Console
 import os
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 console = Console()
-console.print("[bold green italic]Hack Quest")
+clear()
+console.print("[bold green]Hack Quest\n[bold green]----------------------\n[green]Cracking codes like 1998.\n[green italic]by Samy Mohamed")
