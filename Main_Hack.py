8ba063bd1c3eca5be2fd2e277446d59992047275
SamyIOoOI <samytonygo@gmail.com>
2025-10-03 17:32:59 +0300

    map update & code

--- Main_Hack.py
+++ Main_Hack.py
@@ -1,20 +1,26 @@
 from rich import print
 from rich.console import Console
 import os
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-console = Console()
 current_room = 'The HQ'
 Coins = 0
 vowel_var = ['a', 'e', 'i', 'o', 'u']
 inventory = []
+console = Console()
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 def prompt():
-    if not inventory:
-        console.print(f"[bold green]Hack Quest\n[bold green]----------------------\n[green][italic]Cracking codes like 1998.[/italic]\n[bold green]----------------------\n[bold yellow]Inventory: Empty │ Coins: {Coins}")
-    if inventory:
-        console.print(f"[bold green]Hack Quest\n[bold green]----------------------\n[green][italic]Cracking codes like 1998.[/italic]\n[bold green]----------------------\n[bold yellow]Inventory: {inventory} │ coins: {Coins}") 
-map_rooms = { 'The HQ' : {'East' : 'Main Street'},
-             'Main Street' : {'West' : 'The HQ', 'North' : 'Internet Cafe', 'South' : 'ATM', 'East' : 'East Street'},
+    clear()
+    console.print(f"[bold green]Hack Quest\n[bold green]----------------------\n[green][italic]Cracking codes like 1998.[/italic]\n[bold green]----------------------")
+def Game():
+    while True:
+        clear()
+        if not inventory:
+            console.print(f"[bold green]Hack Quest\n[bold green]----------------------\n[green][italic]Cracking codes like 1998.[/italic]\n[bold green]----------------------\n[bold yellow]Inventory: Empty │ Coins: {Coins}")
+        else:
+            console.print(f"[bold green]Hack Quest\n[bold green]----------------------\n[green][italic]Cracking codes like 1998.[/italic]\n[bold green]----------------------\n[bold yellow]Inventory: {inventory} │ coins: {Coins}") 
+
+map_rooms = { 'The HQ' : {'East' : 'Main Street', 'Item': 'Old Laptop'},
+             'Main Street' : {'West' : 'The HQ', 'North' : 'Internet Cafe', 'South' : 'ATM', 'East' : 'East Street', 'item': 'Lost Wallet'},
              'Internet Cafe' : {'South' : 'Main Street'},
              'ATM' : {'North' : 'Main Street'},
              'East Street' : {'West' : 'Main Street', 'East' : 'Bank Entrance', 'North' : 'Apartment', 'South' : 'Shopping Center'},
