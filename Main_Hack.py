0e4c17ddbb2f1355c4a0d0a0fb20d39b321703ce
SamyIOoOI <samytonygo@gmail.com>
2025-10-03 16:25:40 +0300

    Map Update

--- Main_Hack.py
+++ Main_Hack.py
@@ -7,3 +7,15 @@ def clear():
 console = Console()
 clear()
 console.print("[bold green]Hack Quest\n[bold green]----------------------\n[green]Cracking codes like 1998.\n[green italic]by Samy Mohamed")
+map_rooms = { 'The HQ' : {'East' : 'Main Street'},
+             'Main Street' : {'West' : 'The HQ', 'North' : 'Internet Cafe', 'South' : 'ATM', 'East' : 'East Street'},
+             'Internet Cafe' : {'South' : 'Main Street'},
+             'ATM' : {'North' : 'Main Street'},
+             'East Street' : {'West' : 'Main Street', 'East' : 'Bank Entrance', 'North' : 'Apartment', 'South' : 'Shopping Center'},
+             'Apartment' : {'South' : 'East Street'},
+             'Shopping Center' : {'North' : 'East Street'},
+             'Bank Entrance' : {'West' : 'East Street', 'East' : 'Bank Office Room', 'North' : 'Bank Server Room', 'South' : 'Bank Archives Room'},
+             'Bank Archives Room' : {'North' : 'Bank Entrance'},
+             'Bank Server Room' : {'South' : 'Bank Entrance'},
+             'Bank Office Room' : {'West' : 'Bank Entrance', 'East' : 'Vault'},
+             'Vault' : {'West' : 'Bank Office Room'}}
